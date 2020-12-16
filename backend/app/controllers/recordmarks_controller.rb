594e601c70e6bfb6cc41c700c83e272c42a5f67e
aleebrady <aleebrady@gmail.com>
2020-12-16 11:45:09 -0800

    created actions

--- backend/app/controllers/recordmarks_controller.rb
+++ backend/app/controllers/recordmarks_controller.rb
@@ -1,2 +1,12 @@
 class RecordmarksController < ApplicationController
+    
+    def index
+        recordmarks = Recordmark.all
+        render json: recordmarks
+    end
+
+    def show
+        recordmark = Recordmark.find(params[:id])
+        render json: recordmark
+    end
 end
