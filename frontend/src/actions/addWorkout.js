aa28aa0781fb783ed24921bdcb61e9129b1cf61d
aleebrady <aleebrady@gmail.com>
2020-12-17 14:38:09 -0800

    corrected sytax error for POST

--- frontend/src/actions/addWorkout.js
+++ frontend/src/actions/addWorkout.js
@@ -2,8 +2,8 @@ export function addWorkout(data) {
     return (dispatch) => {
         fetch(`http://localhost:3001/workouts`, {
             headers: {
-                'Content-Type': 'application-json',
-                'Accept': 'application-json'
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
             },
             method: 'POST',
             body: JSON.stringify(data)
