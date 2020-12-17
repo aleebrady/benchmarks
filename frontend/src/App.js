a6233ce57d10d70044eaef251a7368814bec004a
aleebrady <aleebrady@gmail.com>
2020-12-17 12:14:56 -0800

    imported files to render

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,40 @@
-// import logo from './logo.svg';
-// import './App.css';
+
 import React from 'react'
+import WorkoutsContainer from './containers/workoutsContainer'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import {Navbar, Nav} from "react-bootstrap"
+import About from './components/about'
+import Home from './components/home'
+import RecordmarksContainer from './containers/recordmarksContainer'
 
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  
+  render() {
+    return (
+      <div className="App">
+        <Router>
+        <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="/">Record Marks</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link as={Link} to="/workouts/new">Create a Workout</Nav.Link>
+            <Nav.Link as={Link} to="/recordmarks">All Recordmarks</Nav.Link>
+            <Nav.Link as={Link} to="/workouts">My Workouts</Nav.Link>
+            <Nav.Link as={Link} to="/about">About</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar><br />
+        <Route path="/about" component={About}/>
+        <Route exact path="/" component={Home}/>
+        <WorkoutsContainer />
+        <Route exact path="/recordmarks" component={RecordmarksContainer}/>
+        </Router>
+      </div>
+    )
+  }
 }
+
+
 
 export default App;
